1b5a47f10f98f1fadc960ab943529cc53224aa7b
lisaevermore <70776430+lisaevermore@users.noreply.github.com>
2023-03-02 14:55:12 +0000

    manager prompt function

--- starter/index.js
+++ starter/index.js
@@ -13,3 +13,36 @@ const render = require("./src/page-template.js");
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+const teamMembers = [];
+
+function promptManager() {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      message: 'Enter the team manager\'s name:',
+      name: 'name',
+    },
+    {
+      type: 'input',
+      message: 'Enter the team manager\'s employee ID:',
+      name: 'id',
+    },
+    {
+      type: 'input',
+      message: 'Enter the team manager\'s email address:',
+      name: 'email',
+    },
+    {
+      type: 'input',
+      message: 'Enter the team manager\'s office number:',
+      name: 'officeNumber',
+    },
+  ]).then((responses) => {
+    const { name, id, email, officeNumber } = responses;
+    const manager = new Manager(name, id, email, officeNumber);
+    teamMembers.push(manager);
+    console.log('Manager added to team roster!');
+  });
+}
+
+promptManager()
